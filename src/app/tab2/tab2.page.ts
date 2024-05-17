68485f119439be3fe7f9919da7577063302f1dbd
Jaime <jaimecaballero99@gmail.com>
2024-05-17 10:50:05 +0200

    Añadimos alert al añadir el item

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {ShoppingItemsService} from "../services/shopping-items.service";
+import {AlertController} from "@ionic/angular";
 
 @Component({
   selector: 'app-tab2',
@@ -11,7 +12,8 @@ export class Tab2Page {
   public item: string;
 
   constructor(
-    private shoppingList: ShoppingItemsService
+    private shoppingList: ShoppingItemsService,
+    private alertController: AlertController
   ) {}
 
   addItem() {
@@ -21,9 +23,27 @@ export class Tab2Page {
       this.shoppingList.addItem(this.item);
       this.item = '';
       console.log(this.shoppingList.items);
-
+      this.alertSuccess();
     } else {
-
+      this.alertError();
     }
+  }
+
+  async alertSuccess(){
+    const alert = await this.alertController.create({
+      header: 'Éxito',
+      message: '¡Item añadido',
+      buttons: ['OK']
+    })
+    await alert.present()
+  }
+
+  async alertError(){
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'El item ya existe',
+      buttons: ['OK']
+    })
+    await alert.present()
   }
 }
